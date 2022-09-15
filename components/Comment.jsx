f62c491ed6dd42dac6d4cd56348a2f55ccc28b6c
Thian <thian.s@Thians-MacBook-Air.local>
2022-09-15 14:30:56 +0700

    finished?

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -5,9 +5,36 @@ export default function Comment(props) {
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div className="d-flex gap-2 my-2">
+          <img
+            src={props.a.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {props.a.username}
+            </span>
+            <br />
+            <span>{props.a.commentText}</span>
+            <span style={{ color: "#E4E6EB" }}>จริงค่า</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{props.a.likeNum} คน</span>
+            </div>
+          </div>
+        </div>
 
       {/*render Reply here... */}
+      {props.a.replies.map((element,id) => (
+        <Reply a = {element} key = {id}/>
+      ))}
+    
     </div>
   );
 }
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import Comment from "./Comment";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div className="vstack gap-3">
+          <div className="d-flex align-items-center gap-3">
+            <img
+              src="/profileImages/Thian.jpg"
+              width="48"
+              height="48"
+              className="rounded-circle"
+              style={{ objectFit: "cover" }}
+            />
+            <span className="fw-semibold fs-5 text-white">
+              Thian Suwannakul 620610176
+            </span>
+          </div>
+          <span className="text-white">
+            ฝนตกให้ใส่เสื้อกันฝน แต่ถ้าสอบตกต้องใส่เสื้ออะไร
+          </span>
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span style={{ color: "#B0B3B8" }}>1M คน</span>
+          </div>
+          <hr className="m-0 border" />
+          {props.comments.map(
+            (a,id) => {
+            return <Comment a = {a} key={id}/>
+            }
+          )
+          }
+        </div>
+  );
 }
